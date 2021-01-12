20a3e978d3b669af6ff28bafdc7b3431b27e2bb0
rumblekat <reki318@naver.com>
2021-01-12 21:19:08 +0900

    first commit for vapor

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,14 @@
 import Fluent
 import Vapor
 
+struct InfoData : Content{
+    let name : String
+}
+
+struct InfoResponse : Content{
+    let request : InfoData
+}
+
 func routes(_ app: Application) throws {
     app.get { req in
         return req.view.render("index", ["title": "Hello Vapor!"])
@@ -10,5 +18,17 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
 
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else{
+            throw Abort(.internalServerError)
+        }
+        return "Hello, \(name)"
+    }
+    
+    app.post("info") { req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(request: data)
+    }
+    
     try app.register(collection: TodoController())
 }
